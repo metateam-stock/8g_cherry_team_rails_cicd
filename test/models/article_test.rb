e7ce241f2044f7d2d849612c4ddd7e4b67cc126d
HninHninYu1495 <hninhninyu@seattleconsulting.co.jp>
2023-03-11 00:41:15 +0900

    Rails testing

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should not save article without title" do
+		article = Article.new(body: "text text text")
+		assert_not article.save, "should not save article without title"
+	end
+	
+	test "should save body characters minimum 10" do
+		article = Article.new(title: "title", body: "body")
+		assert_not article.save, "should save body characters minimum 10"
+	end
+
+	test "should save article with valid parameter" do
+		article = Article.new(title: "title", body: "body of the text")
+		assert article.save, "should save article with valid parameter"
+	end
+	
 end
